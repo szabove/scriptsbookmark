315ef8dfa792a1063275339e64b4dd940fcd16c9
szabove <szabove@gmail.com>
2020-02-28 10:09:24 +0100

    Adding Bookmarks to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ScriptsBookmark.Models;
 
 namespace ScriptsBookmark.Data
 {
@@ -12,5 +13,9 @@ namespace ScriptsBookmark.Data
             : base(options)
         {
         }
+
+
+        public DbSet<Bookmark> Bookmark { get; set; } 
+
     }
 }
